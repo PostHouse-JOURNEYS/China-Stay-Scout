08ccebdd13c10503bb8a5af0b43027d8eb180e49
PostHouse-JOURNEYS <wangyuxin02@gmail.com>
2026-04-08 20:41:45 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "PostHouse-JOURNEYS.github.io/China-Stay-Scout",
+    baseUrl: "https://PostHouse-JOURNEYS.github.io/China-Stay-Scout",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
